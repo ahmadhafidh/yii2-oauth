f4b991f5064b36b6a51cbaa9aa1faaf2dd20f3cd
ahmad.hafidh <ahmad.ayatullah@spesolution.com>
2020-04-26 23:58:01 +0700

    commit yii2 oauth fb,twitter,gmail

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=acme',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
